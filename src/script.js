40ed9f0749d5846210579e571e7262a880805af3
packagespace <packagespace@pm.me>
2022-04-22 22:52:50 +0200

    single file because require is annoying

--- src/script.js
+++ src/script.js
@@ -1,9 +1,11 @@
-const KICKSTARTER_JSON = https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/kickstarter-funding-data.json;
+const KICKSTARTER_JSON = "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/kickstarter-funding-data.json";
 
-import { Treemap } from "./Treemap";
+function Treemap(data, options){
+    console.log(data);
+};
 
 const options = {
-    
+
 }
 
 d3.json(KICKSTARTER_JSON).then(data => Treemap(data, options));

f5d2e94b470c5917c6a38951e1dc5d687ddce0d1
packagespace <packagespace@pm.me>
2022-04-22 22:44:11 +0200

    setup done, started working

--- src/script.js
+++ src/script.js
@@ -1,1 +1,9 @@
-const fn = () => 1;
+const KICKSTARTER_JSON = https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/kickstarter-funding-data.json;
+
+import { Treemap } from "./Treemap";
+
+const options = {
+    
+}
+
+d3.json(KICKSTARTER_JSON).then(data => Treemap(data, options));
